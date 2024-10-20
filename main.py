3b6b5f1d3387c7c450bc0c5913abffcf77917cb4
JT <jstorbit@gmail.com>
2024-10-20 17:43:00 -0600

    created other functions for etch and sketch

--- main.py
+++ main.py
@@ -4,8 +4,26 @@ turt = Turtle()
 screen = Screen()
 
 def move_forward():
-   turt.forward(10)
+   turt.forward(15)
+
+def move_backward():
+   turt.backward(15)
+
+def turn_counter_clockwise():
+   turt.left(15)
+
+def turn_clockwise():
+   turt.right(15)
+
+def clear_screen():
+   turt.home()
+   turt.clear()
+   
 
 screen.listen()
-screen.onkey(move_forward, "space")
+screen.onkey(move_forward, "w")
+screen.onkey(move_backward, "s")
+screen.onkey(turn_counter_clockwise, "a")
+screen.onkey(turn_clockwise, "d")
+screen.onkey(clear_screen, "c")
 screen.exitonclick()
